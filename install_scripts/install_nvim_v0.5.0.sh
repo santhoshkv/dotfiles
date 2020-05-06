758526b0fd9a441d47bebe426c8217f183045d65
Santhosh.Vijayan <santhosh.vijayan@nutanix.com>
2020-05-06 00:22:32 -0700

    Bump nvim version, script fixes

--- install_scripts/install_nvim_v0.5.0.sh
+++ install_scripts/install_nvim_v0.5.0.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
-if type nvim > /dev/null 2>&1; then
 cd $HOME
-wget https://github.com/neovim/neovim/releases/download/v0.4.2/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 ./nvim.appimage --appimage-extract
 cp -r squashfs-root/usr/share/nvim/ /usr/share/
 cp squashfs-root/usr/bin/nvim /usr/bin/
 rm -rf squashfs
 rm nvim.appimage*
-source /epsilon/venv/bin/activate
-pip install --upgrade pynvim && pip3 install --upgrade pynvim
+source /venv/bin/activate
+pip install --upgrade pynvim
 mkdir -p $HOME/.config/nvim/
 touch $HOME/.config/nvim/init.vim
 cat > $HOME/.config/nvim/init.vim <<EOF
@@ -17,4 +16,3 @@ set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/dotfiles/nvim/.vimrc
 EOF
-fi
